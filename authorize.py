86229940777bbed20f2b82d13a94971fdcd15856
José Manuel Delicado Alcolea <jmdaweb@hotmail.com>
2023-04-18 09:40:14 +0200

    Se cambia el nombre de fediinforma a fedifeed en authorize.py

--- authorize.py
+++ authorize.py
@@ -2,7 +2,7 @@ from mastodon import Mastodon
 
 print("Rellena la información necesaria para obtener tu token de acceso.")
 instance_url = input("URL de la instancia: ")
-client_id, client_secret = Mastodon.create_app("Fediinforma", api_base_url=instance_url, website="https://nvda.es")
+client_id, client_secret = Mastodon.create_app("Fedifeed", api_base_url=instance_url, website="https://fedifeed.net")
 temporary_api = Mastodon(client_id=client_id, client_secret=client_secret, api_base_url=instance_url, user_agent="Fediinforma/1.0")
 auth_url = temporary_api.auth_request_url()
 print("Genial, parece que todo ha ido bien. Ahora, visita esta URL en tu navegador web y autoriza la app en tu cuenta")
